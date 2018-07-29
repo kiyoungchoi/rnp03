6947e4627ffca7f5cede70aacc53a683918004c7
kiyoungchoi <kaist6123@naver.com>
2018-07-29 15:49:37 +0900

    worked

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { ApolloClient, HttpLink, InMemoryCache } from 'apollo-client-preset'
 import ListPage from './components/ListPage'
 
 // __SIMPLE_API_ENDPOINT__ looks like: 'https://api.graph.cool/simple/v1/__SERVICE_ID__'
-const httpLink = new HttpLink({ uri: '__SIMPLE_API_ENDPOINT__' })
+const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cjk6hcn714rm70158ljupeswk' })
 
 const client = new ApolloClient({
   link: httpLink,
